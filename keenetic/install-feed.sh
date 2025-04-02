a3c157992097a703dc05dc8ae1e01c007353e623
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-04-02 19:45:20 +0300

    Update install-feed.sh

--- keenetic/install-feed.sh
+++ keenetic/install-feed.sh
@@ -4,7 +4,7 @@ echo "Updating package list..."
 opkg update
 
 echo "Installing wget with HTTPS support..."
-opkg install wget-ssl
+opkg install wget-ssl curl vim nano
 opkg remove wget-nossl
 
 echo "Detecting system architecture (via opkg)..."

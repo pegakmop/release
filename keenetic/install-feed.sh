cab104095ceb7e63b8891fd3887ac5e38a722fb8
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-04-02 19:43:53 +0300

    Update install-feed.sh

--- keenetic/install-feed.sh
+++ keenetic/install-feed.sh
@@ -21,13 +21,13 @@ fi
 
 case "$ARCH" in
   aarch64-3.10)
-    FEED_URL="https://ground-zerro.github.io/release/keenetic/aarch64-k3.10"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/aarch64-k3.10"
     ;;
   mipsel-3.4)
-    FEED_URL="https://ground-zerro.github.io/release/keenetic/mipselsf-k3.4"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/mipselsf-k3.4"
     ;;
   mips-3.4)
-    FEED_URL="https://ground-zerro.github.io/release/keenetic/mipssf-k3.4"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/mipssf-k3.4"
     ;;
   *)
     echo "Unsupported architecture: $ARCH"
@@ -39,7 +39,7 @@ echo "Architecture detected: $ARCH"
 echo "Selected feed: $FEED_URL"
 
 FEED_CONF="/opt/etc/opkg/customfeeds.conf"
-FEED_LINE="src/gz ground-zerro $FEED_URL"
+FEED_LINE="src/gz pegakmop $FEED_URL"
 
 # Ensure the opkg directory exists
 if [ ! -d "/opt/etc/opkg" ]; then

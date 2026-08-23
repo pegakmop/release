c37abb9d2098f3e555efbfbc798ad36aee675a22
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-08-23 12:45:01 +0300

    Update opkg.sh

--- keenetic/opkg.sh
+++ keenetic/opkg.sh
@@ -24,13 +24,13 @@ fi
 
 case "$ARCH" in
   aarch64-3.10)
-    FEED_URL="https://www.pegakmop.site/release/keenetic/aarch64-k3.10"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/aarch64-k3.10"
     ;;
   mipsel-3.4)
-    FEED_URL="https://www.pegakmop.site/release/keenetic/mipselsf-k3.4"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/mipselsf-k3.4"
     ;;
   mips-3.4)
-    FEED_URL="https://www.pegakmop.site/release/keenetic/mipssf-k3.4"
+    FEED_URL="https://pegakmop.github.io/release/keenetic/mipssf-k3.4"
     ;;
   *)
     echo "Не поддерживаемая архитектура: $ARCH"
@@ -42,7 +42,7 @@ echo "Определена архитектура: $ARCH"
 echo "Устанавливаю репозиторий: $FEED_URL"
 
 FEED_CONF="/opt/etc/opkg/neofit.conf"
-FEED_LINE="src/gz pegakmop $FEED_URL"
+FEED_LINE="src/gz neofit $FEED_URL"
 
 # Ensure the opkg directory exists
 if [ ! -d "/opt/etc/opkg" ]; then
